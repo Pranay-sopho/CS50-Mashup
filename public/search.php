099bf359ad1b17bbba04de62df22162041103d96
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-01-04 14:41:42 +0000

    Search Completed

--- public/search.php
+++ public/search.php
@@ -5,7 +5,21 @@
     // numerically indexed array of places
     $places = [];
 
-    // TODO: search database for places matching $_GET["geo"], store in $places
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name) AGAINST (?)", $_GET["geo"]);
+    
+    // implements the autocomplete for postal codes and place name
+    if (count($places) == 0)
+    {
+        $places = CS50::query("SELECT * FROM places WHERE place_name LIKE ?", $_GET["geo"] . "%");
+        if (count($places) == 0)
+        {
+            $places = CS50::query("SELECT * FROM places WHERE postal_code LIKE ?", $_GET["geo"] . "%");
+            if (count($places) == 0)
+            {
+                $places = CS50::query("SELECT * FROM places WHERE admin_name1 = ?", $_GET["geo"]);
+            }
+        }
+    }
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
